cf1ae19ecf00244f27e22c0ea48f9e0cd4d6f62a
Rainer Englisch <rainer.englisch@gmx.de>
2016-01-20 06:13:18 +0100

    Added functionality for creation of a date cell for every matchday

--- src/main/java/email/englisch/MatchdayToXlsxConverter.java
+++ src/main/java/email/englisch/MatchdayToXlsxConverter.java
@@ -1,14 +1,17 @@
 package email.englisch;
 
 import javafx.scene.control.Alert;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -27,12 +30,32 @@ public class MatchdayToXlsxConverter {
      */
     public static void createXlsxFrom(final Stream<Matchday> matchdayStream) {
         Objects.requireNonNull(matchdayStream, "matchdayStream must not be null");
-        createXlsxFile();
+        createXlsxFile(matchdayStream);
     }
 
-    private static void createXlsxFile() {
+    private static void createXlsxFile(final Stream<Matchday> matchdayStream) {
         final Workbook wb = new XSSFWorkbook();
         final Sheet sheet = wb.createSheet("Spielplan");
+
+        final List<Matchday> matchdays = matchdayStream.collect(Collectors.toList());
+        final CreationHelper creationHelper = wb.getCreationHelper();
+        final CellStyle dateCellStyle = wb.createCellStyle();
+        dateCellStyle.setDataFormat(creationHelper.createDataFormat().getFormat("ddd, dd.mm.yy"));
+        int rowno = 0;
+        for (Matchday matchday : matchdays) {
+            final LocalDate matchdayDate = matchday.getDate();
+            final Calendar calendar = Calendar.getInstance();
+            calendar.set(
+                    matchdayDate.getYear(),
+                    matchdayDate.getMonthValue()-1,
+                    matchdayDate.getDayOfMonth());
+            final Row row = sheet.createRow(rowno);
+            rowno++;
+            final Cell cell = row.createCell(0);
+            cell.setCellValue(calendar);
+            cell.setCellStyle(dateCellStyle);
+        }
+
         try (final FileOutputStream fileOut = new FileOutputStream("spielplan.xlsx")) {
             wb.write(fileOut);
             fileOut.close();
@@ -44,7 +67,6 @@ public class MatchdayToXlsxConverter {
         catch (final IOException e) {
             final Alert alert = new Alert(Alert.AlertType.ERROR, "Fehler beim Schreiben in die XLSX-Datei!");
             alert.showAndWait();
-
         }
     }
 }
